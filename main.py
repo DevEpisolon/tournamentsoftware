a8918f8f3edb5a66e363d2450844c546a9c4a29e
ShivManek <shivkumarmanek@gmail.com>
2024-04-11 20:44:12 -0700

    new main

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
+import player_database
 from tournament import Tournament
 from player import *
-from testing_player import *
 from player import Player
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pymongo import MongoClient
-
-#need to change this to the mongodb with fastapi
-tournaments = []
-
-
-#redact it and update it 
-def add_players():
-    # Create players for the tournament
-    players = []
-    for i in dummies:
-        players.append(i)
-    return players
+from player_database import *
+import asyncio
 
 
 def create_tournament():
@@ -39,20 +29,30 @@ def create_tournament():
         tournamentWinner=None,
         droppedPlayers=None
     )
-    # Create players for the tournament
-    # fix this part
-    players = dummies[:]
+    return tournament
+
+def collect_players(tournament):
+    players = []
+    size = 8
+    players_in_tournament = 0
+    while(players_in_tournament < size):
+        add_player = input("Enter your display name: ")
+        #get player from database
+        kk = player_database.get_player(add_player)
+        players.append(add_player)
+        players_in_tournament += 1
     tournament.set_Players(players)
     print([p.get_displayname() for p in players])
+
+def play_tournament(tournament):
+
     tournament.createMatches()
     print()
-    #This shows only the match object because it is in a list
-    #print(tournament.get_Matches())
-    print()
-    tournaments.append(tournament)
-    #This shows the match info from the __str__ method
+    # This shows only the match object because it is in a list
+    # print(tournament.get_Matches())
+    # This shows the match info from the __str__ method
     tournament.viewMatchesinTournament()
-    #print("Reached end")
+    # print("Reached end")
     while (tournament.get_tournamentWinner() is None):
         mid = input("\nEnter Match ID you would like to view: ")
         if int(mid) == len(tournament.get_Matches()):
@@ -79,36 +79,36 @@ def create_tournament():
     # update player info with match history
     print("Thank you for playing!!!")
 
+def update_player_info():
+    pass
+
 
 def main():
-    selection = 69
-    while(selection is not 0):
-        selection = input("Select what would you like to do?\n" +
-                 "1: Create a tournament\n" +
-                 "2: View tournaments\n" +
-                 "3: Add a player\n" +
-                 "4: Delete a player\n" +
-                 "5: Update player info\n" +
-                 "0: Quit")
+    running = True
+    while (running):
+        running = int(input("Select what would you like to do?\n" +
+                    "1: Create a tournament\n" +
+                    "2: collect players for tournament\n" +
+                    "3: View tournaments\n" +
+                    "4: Update player info\n" +
+                    "0: Quit\n"
+                    "your choice: "))
+        if (running == 1):
+            current_tournament = create_tournament()
+            print("\nEmpty tournament created\n")
+        elif(running == 2):
+            print("\nCollecting players for tournament\n")
+            collect_players(current_tournament)
+            print("\nplayers added to tournament\n")
+        elif (running == 3):
+            print("starting tournament...")
+            play_tournament(current_tournament)
+        elif (running == 4):
+            update_player_info()
+        elif (running == 0):
+            running = False
 
-        if(selection ==1):
-           create_tournament()
-        elif(selection ==2):
-            # view the tournaments ongoing and update them?
-            print([tournament for tournament in tournaments])
-        elif(selection ==3):
-            #add a player
-            #This should be for the database but idk if its created or how we can do it     
-            name = input("Enter players name")
-            display = input("Enter players displayname")
-            player = Player(playername=playername,displayname=display)
-            print(f"Player {player.get_displayname()} has been created!")
-        elif(selection ==4):
-            #Delete a player
-        elif(selection ==5):
-            #update player info
-        else:
-             break
+
 
 
 
