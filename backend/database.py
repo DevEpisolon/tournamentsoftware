efbb515909f5253de8b842d7545df583b76e602c
SeungJBaik <119748522+SeungJBaik@users.noreply.github.com>
2024-04-25 18:20:07 -0700

    Add files via upload

---
+++ backend/database.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://seungbaik:qazqaz213@cluster0.9ziumia.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.tournament_software
+
+player_collection = db["player"]
+match_collection = db["match"]
