e191f7549e76ca1645b2dc01f8adda13abf2b10d
epii <blockproductions123@gmail.com>
2024-05-15 21:20:56 -0700

    working frontend needs adjustment

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import PlayerProfilePage from './components/PlayerProfilePage';
-import SingleElimination from './components/single-elimination';
-
+import TournamentPage from './components/TournamentPage'; // Import TournamentPage component
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/player/:playername" element={<PlayerProfilePage />} />
-      <Route path="/tournament/663ebbbddc53254338df8862" element={<SingleElimination />} />
-    </Routes>
+      {/* Add :tournamentId parameter */}
+    <Route path="/tournament/:tournamentId" element={<TournamentPage />}/>
+   </Routes>
   </Router>,
   document.getElementById('root')
 );
