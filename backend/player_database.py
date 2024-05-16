3893b7786fe9fb0f13fd1c7929a208e797dff8df
Aadim1 <iamaadim5@gmail.com>
2024-05-15 19:36:55 -0700

    MongoDB One Source (#73)
    * Added requirements.txt and made MongoDB one source of truth for database
    
    * Added CRUD match, and exception handlers

--- backend/player_database.py
+++ backend/player_database.py
@@ -1,17 +1,19 @@
 import asyncio
 from fastapi import APIRouter, HTTPException
 from pymongo import MongoClient
+from mongo import MongoDB
 from player import Player
 
 # Initialize FastAPI app
 player_router = APIRouter()
 
-# MongoDB Atlas connection string
-MONGODB_CONNECTION_STRING = "mongodb+srv://tas32admin:onward508@tournamentsoftware.l9dyjo7.mongodb.net/?retryWrites=true&w=majority&appName=tournamentsoftware"
-# Replace <username>, <password>, <cluster-url>, and <dbname> with your actual MongoDB Atlas credentials and database name
+# # MongoDB Atlas connection string
+# MONGODB_CONNECTION_STRING = "mongodb+srv://tas32admin:onward508@tournamentsoftware.l9dyjo7.mongodb.net/?retryWrites=true&w=majority&appName=tournamentsoftware"
+# # Replace <username>, <password>, <cluster-url>, and <dbname> with your actual MongoDB Atlas credentials and database name
 
-# Establish connection to MongoDB Atlas
-client = MongoClient(MONGODB_CONNECTION_STRING)
+# # Establish connection to MongoDB Atlas
+# client = MongoClient(MONGODB_CONNECTION_STRING)
+client = MongoDB().getDb()
 db = client["tournamentsoftware"]
 players_collection = db["players"]
 
