21b4f580a49b1b6af13a9c35ed4c0bd454d08c77
epii <blockproductions123@gmail.com>
2024-05-15 19:25:27 -0700

    profiles work

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import PlayerProfilePage from './components/PlayerProfilePage'; // Import the PlayerProfilePage component
+import PlayerProfilePage from './components/PlayerProfilePage';
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/stats/:displayName" element={<PlayerProfilePage />} />
+      <Route path="/player/:playername" element={<PlayerProfilePage />} />
     </Routes>
   </Router>,
   document.getElementById('root')
