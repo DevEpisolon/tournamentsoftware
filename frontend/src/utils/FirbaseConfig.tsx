18416406290461196ae0a4ba1939d916650a6740
Timmy6158 <123699196+Timmy6158@users.noreply.github.com>
2024-07-23 04:17:58 -0700

    Update FirbaseConfig.tsx

--- frontend/src/utils/FirbaseConfig.tsx
+++ frontend/src/utils/FirbaseConfig.tsx
@@ -1,26 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyB2rcpW959s25Kc9XEMJla03FULyC3I7ZU",
-  authDomain: "tournament-f8373.firebaseapp.com",
-  projectId: "tournament-f8373",
-  storageBucket: "tournament-f8373.appspot.com",
-  messagingSenderId: "525160333112",
-  appId: "1:525160333112:web:f01ba8fbbb47a5f0df0bcd"
-};
-*/
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB2rcpW959s25Kc9XEMJla03FULyC3I7ZU",
-  authDomain: "tournament-f8373.firebaseapp.com",
-  projectId: "tournament-f8373",
-  storageBucket: "tournament-f8373.appspot.com",
-  messagingSenderId: "525160333112",
-  appId: "1:525160333112:web:f01ba8fbbb47a5f0df0bcd"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
