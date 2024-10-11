e23283cf4cf9c81b3d7da9f658074f6662b144ba
epii <blockproductions123@gmail.com>
2024-10-10 20:11:33 -0700

    profile page

--- frontend/src/components/PlayerProfilePage.tsx
+++ frontend/src/components/PlayerProfilePage.tsx
@@ -47,43 +47,61 @@ const PlayerProfilePage: React.FC = () => {
   const playerImage = playerData.avatar || DEFAULT_IMAGE_URL;
 
   return (
-    <div className="container mx-auto mt-8 relative">
-      <button
-        className="absolute top-3 left-4 bg-pink-700 font-bold text-white px-3 py-2 rounded"
-        onClick={handleGoBack}
-      >
-        Back
-      </button>
-      <header className="text-center mb-8">
-        <img
-          src={playerImage}
-          alt="Player Avatar"
-          className="rounded-full w-32 h-32 mx-auto mb-4 border-4 border-black"
-        />
-        <h1 className="text-4xl font-bold">{playerData.displayname}</h1>
-        <p className="text-sm italic">Joined: {playerData.join_date}</p>
-      </header>
-      <div className=" border-4 border-black bg-gray-100 p-4 rounded-md shadow-md">
-        <h2 className="text-lg font-semibold mb-2">About Me</h2>
-        {/* Add more player details as needed */}
-      </div>
-      <div className="grid grid-cols-2 gap-4 mt-8">
-        <div className="border-custom-border border-black g-blue-100 p-2 rounded-md shadow-md transition-transform duration-300 hover:scale-105">
-          <h2 className="text-lg font-semibold mb-2">Wins</h2>
-          <p className="text-xl">{playerData.wins}</p>
+    <div className="bg-[#2D3250] min-h-screen text-white">
+      <div className="container mx-auto pt-8 relative">
+        <button
+          className="absolute top-3 left-4 bg-pink-700 font-bold text-white px-3 py-2 rounded"
+          onClick={handleGoBack}
+        >
+          Back
+        </button>
+        <header className="text-center mb-8">
+          <img
+            src={playerImage}
+            alt="Player Avatar"
+            className="rounded-full w-32 h-32 mx-auto mb-4 border-4 border-white transition-transform duration-300 hover:scale-105 hover:filter hover:grayscale"
+          />
+          <h1 className="text-4xl font-bold">{playerData.displayname}</h1>
+          <p className="text-sm italic">Joined: {playerData.join_date}</p>
+        </header>
 
+        <div className="border-4 border-white bg-gray-100 text-black p-4 rounded-md shadow-md">
+          <h2 className="text-lg font-semibold mb-2">About Me</h2>
+          {/* Add more player details as needed */}
         </div>
-        <div className="border-4 border-black g-green-100 p-2 rounded-md shadow-md transition-transform duration-300 hover:scale-105">
-          <h2 className="text-lg font-semibold mb-2">Losses</h2>
-          <p className="text-xl">{playerData.losses}</p>
-        </div>
-        <div className="border-4 border-black g-yellow-100 p-2 rounded-md shadow-md transition-transform duration-300 hover:scale-105">
-          <h2 className="text-lg font-semibold mb-2">Ties</h2>
-          <p className="text-xl">{playerData.ties}</p>
-        </div>
-        <div className="border-4 border-black g-blue-100 p-2 rounded-md shadow-md transition-transform duration-300 hover:scale-105">
-          <h2 className="text-lg font-semibold mb-2">Winstreak</h2>
-          <p className="text-xl">{playerData.winstreaks}</p>
+
+        <div className="grid grid-cols-2 gap-32 mt-12">
+          {/* Diamond shape for Wins (Red) */}
+          <div className="transform rotate-45 border-4 border-white w-36 h-36 mx-auto flex items-center justify-center bg-red-500 shadow-md transition-transform duration-300 hover:scale-110">
+            <div className="transform -rotate-45 text-center">
+              <h2 className="text-lg font-semibold">Wins</h2>
+              <p className="text-xl">{playerData.wins}</p>
+            </div>
+          </div>
+
+          {/* Diamond shape for Losses (Blue) */}
+          <div className="transform rotate-45 border-4 border-white w-36 h-36 mx-auto flex items-center justify-center bg-blue-500 shadow-md transition-transform duration-300 hover:scale-110">
+            <div className="transform -rotate-45 text-center">
+              <h2 className="text-lg font-semibold">Losses</h2>
+              <p className="text-xl">{playerData.losses}</p>
+            </div>
+          </div>
+
+          {/* Diamond shape for Ties (#F6B17A) */}
+          <div className="transform rotate-45 border-4 border-white w-36 h-36 mx-auto flex items-center justify-center bg-[#F6B17A] shadow-md transition-transform duration-300 hover:scale-110">
+            <div className="transform -rotate-45 text-center">
+              <h2 className="text-lg font-semibold">Ties</h2>
+              <p className="text-xl">{playerData.ties}</p>
+            </div>
+          </div>
+
+          {/* Diamond shape for Winstreak (Green) */}
+          <div className="transform rotate-45 border-4 border-white w-36 h-36 mx-auto flex items-center justify-center bg-green-500 shadow-md transition-transform duration-300 hover:scale-110">
+            <div className="transform -rotate-45 text-center">
+              <h2 className="text-lg font-semibold">Winstreak</h2>
+              <p className="text-xl">{playerData.winstreaks}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -91,3 +109,4 @@ const PlayerProfilePage: React.FC = () => {
 };
 
 export default PlayerProfilePage;
+
