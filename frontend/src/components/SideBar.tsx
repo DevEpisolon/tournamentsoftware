76e22e543ea40813cb9936c5c35f6ef0d82b0f7f
Timmy6158 <roomshocker@gmail.com>
2024-10-17 00:54:20 -0700

    Replaced button for anchor in navigation

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -10,7 +10,7 @@ interface SideBarProps {
 interface ItemProps {
   icon: React.ReactNode;
   text: string;
-  link: string;
+  link?: string;
   active?: boolean;
   alert?: boolean;
 };
@@ -22,7 +22,7 @@ export default function SideBar({ children }: SideBarProps): React.ReactNode {
 
   return (
     <aside className="h-screen sticky self-start top-0 transition-width z-20">
-      <nav className="h-full flex flex-col bg-tourney-navy1 border-r shadow-sm border-tourney-navy2">
+      <nav className="h-full flex flex-col bg-tourney-navy2 border-r shadow-md border-tourney-navy1">
         <div className="p-4 pb-2 flex justify-between items-center">
           <span className={`overflow-hidden transition-all ${expanded ? "w-32" : "w-0"}`}>
             TAS-32
@@ -36,7 +36,7 @@ export default function SideBar({ children }: SideBarProps): React.ReactNode {
             {children}
           </ul>
         </SideBarContext.Provider>
-        <div className='flex border-t p-3 border-tourney-navy2'>
+        <div className='flex border-t p-3 border-tourney-navy1'>
           <img
             //TODO: Change src to player avatar or default image
             src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
@@ -48,7 +48,9 @@ export default function SideBar({ children }: SideBarProps): React.ReactNode {
               <h4 className='font-semibold'>Whoami</h4>
               <span className='text-xs text-gray-50'>random@email.com</span>
             </div>
-            <MdMoreVert size={20} />
+            <button className='hover:bg-tourney-navy3 rounded-md'>
+              <MdMoreVert size={20} />
+            </button>
           </div>
         </div>
       </nav>
@@ -58,15 +60,11 @@ export default function SideBar({ children }: SideBarProps): React.ReactNode {
 
 export function SideBarItem({ icon, text, link, active, alert }: ItemProps): React.ReactNode {
   const { expanded } = useContext(SideBarContext);
-  const navigate = useNavigate();
-  const handleNav = (link: string) => {
-    navigate(link)
-  };
 
   return (
-    <button onClick={() => handleNav(link)} className={`relative flex items-center my-1 px-3 py-2 font-medium rounded-md cursor-pointer transition-colors group
+    <a href={link} className={`relative flex items-center my-1 px-3 py-2 font-medium rounded-md cursor-pointer transition-colors group
       ${active
-        ? "bg-gradient-to-t from-tourney-navy2 to-tourney-navy3 text-tourney-orange"
+        ? "bg-tourney-navy1 text-tourney-orange"
         : "hover:bg-tourney-navy3 text-white"
       }
     `}>
@@ -76,8 +74,8 @@ export function SideBarItem({ icon, text, link, active, alert }: ItemProps): Rea
         <div className={`absolute right-2 w-2 h-2 rounded bg-white ${expanded ? "" : "top-2"}`} />
       )}
 
-      {!expanded && <div className='absolute left-full rounded-md px-5 py-1 bg-tourney-navy3 text-tourney-orange text-sm invisible opacity-20
+      {!expanded && <div className='absolute left-full rounded-md px-5 py-1 bg-tourney-navy3 text-white text-sm invisible opacity-20
         translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-5'>{text}</div>}
-    </button>
+    </a>
   )
 };
